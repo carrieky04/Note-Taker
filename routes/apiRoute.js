15d94d4350145d67bfcdf6ce76a88667202344e6
Carrie Young <carrieky04@gmail.com>
2022-12-13 13:24:56 -0800

    adds working delete note function

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,5 +1,4 @@
 const api = require('express').Router();
-
 const { readFromFile, readAndAppend, writeToFile } = require("../helpers/fsUtils");
 const uuid = require("../helpers/uuid");
 
@@ -32,24 +31,11 @@ api.post('/notes', (req, res) => {
     }
 });
 
-// GET Route for a specific note
-api.get('/:id', (req, res) => {
-    const noteId = req.params.id;
-    readFromFile('./db/db.json')
-      .then((data) => JSON.parse(data))
-      .then((json) => {
-        const noteArray = json.filter((note) => note.id === noteId);
-        return noteArray.length > 0
-          ? res.json(noteArray)
-          : res.json('No note with that ID');
-      });
-  });
-
 // DELETE /api/notes/:id should receive a query parameter that contains the id
 // of a note to delete. To delete a note, you'll need to read all notes from 
 // the db.json file, remove the note with the given id property, and then rewrite
 // the notes to the db.json file.
-api.delete('/:id', (req, res) => {
+api.delete('/notes/:id', (req, res) => {
     const noteId = req.params.id;
     readFromFile('./db/db.json')
     .then((data) => JSON.parse(data))
